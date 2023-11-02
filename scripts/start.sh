764a3d1bbd2cb9b4b5430653b284f89756595617
Inderjot Singh <inderjotsingh141@gmail.com>
2023-11-02 23:46:03 +0530

    added appspec and scripts

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 docker pull inderharrysingh/react-app:latest 
 
 
-docker run -d  -p 9000:80 --name react-app  inderharrysingh/react:latest
+docker run -d  -p 9000:80 --name react-app  inderharrysingh/react-app:latest
 
 
